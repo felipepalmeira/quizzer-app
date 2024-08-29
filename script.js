b9f81fafe4d71b676013861c05921bf630178764
Felipe Palmeira <pro.felipefeitosa.jobs@gmail.com>
2024-08-29 19:30:20 -0300

    adiciona funcionalidades no arquivo script.jss

---
+++ script.js
@@ -0,0 +1,69 @@
+
+let currentQuestionIndex = 0;
+
+
+let questions = [];
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    fetch('questions.json')
+        .then(response => response.json())
+        .then(data => {
+            questions = data.questions;
+            showQuestion();
+        });
+
+
+    document.getElementById('next-btn').addEventListener('click', () => {
+        currentQuestionIndex++;
+        if (currentQuestionIndex < questions.length) {
+            showQuestion();
+        } else {
+            showResults();
+        }
+    });
+});
+
+
+function showQuestion() {
+
+    const questionElement = document.getElementById('question');
+    const optionsElement = document.getElementById('options');
+
+
+    const currentQuestion = questions[currentQuestionIndex];
+
+
+    questionElement.textContent = currentQuestion.question;
+
+
+    optionsElement.innerHTML = '';
+
+
+    currentQuestion.options.forEach(option => {
+        const li = document.createElement('li');
+        li.textContent = option;
+        li.addEventListener('click', () => selectAnswer(option));
+        optionsElement.appendChild(li);
+    });
+}
+
+
+function selectAnswer(answer) {
+    const currentQuestion = questions[currentQuestionIndex];
+
+    if (answer === currentQuestion.answer) {
+        alert('Correct!');
+    } else {
+        alert('Wrong!');
+    }
+
+
+    document.getElementById('next-btn').disabled = false;
+}
+
+
+function showResults() {
+    alert('Quiz completed!');
+}
