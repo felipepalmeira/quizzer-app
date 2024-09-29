e335bd24577a0bdb268c684b0b4b536d889cc0d6
Rodrigo Costa Ronchi <rodrigoronchi33@gmail.com>
2024-09-29 15:09:39 -0300

    Projeto utilizando estrutura Java com servidor Tomcat

--- src/main/java/database/DatabaseConnection.java
+++ src/main/java/database/DatabaseConnection.java
@@ -11,16 +11,16 @@ public class DatabaseConnection {
 
     // Detalhes da conexão (ajuste conforme necessário)
     private static final String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl"; // URL específica para Oracle
-    private static final String USER = "";
-    private static final String PASSWORD = "";
+    private static final String USER = ""; // Preencha com o usuário do banco de dados
+    private static final String PASSWORD = ""; // Preencha com a senha do banco de dados
 
     // Método público estático para obter a conexão
     public static Connection getConnection() {
         if (connection == null) {
             try {
-                // Registrar o driver do banco de dados
-                Class.forName("com.mysql.cj.jdbc.Driver"); // Para MySQL. Substitua se necessário.
-                
+                // Registrar o driver do banco de dados Oracle
+                Class.forName("oracle.jdbc.driver.OracleDriver");
+
                 // Estabelecer a conexão
                 connection = DriverManager.getConnection(URL, USER, PASSWORD);
                 System.out.println("Conexão estabelecida com sucesso.");
